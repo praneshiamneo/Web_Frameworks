04db3230eb135bef3a3f4aa8b080955cf6121255
praneshiamneo <163287540+praneshiamneo@users.noreply.github.com>
2024-04-22 12:34:12 +0530

    Delete DAY - 8 (one to one)/class_exercises directory

--- DAY - 8 (one to one)/class_exercises/src/main/java/com/example/class_exercises/Repository/AddressRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.class_exercises.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.class_exercises.Model.Address;
-
-public interface AddressRepo extends JpaRepository<Address, Long> {
-
-}
